3bbee8732f0db2ff2a838cf6bf272cd1c4eb4c49
Stefan J. Wernli <swernli@microsoft.com>
2020-12-17 10:11:59 -0800

    Adjust move script again

--- Move-LlvmBuild.ps1
+++ Move-LlvmBuild.ps1
@@ -42,7 +42,7 @@ function Copy-Tree ([string]$source, [string]$dest, [string[]]$filter = @("*"), 
 
 function Move-Tree ([string]$source, [string]$dest, [string[]]$filter = @("*"), [string[]]$exclude = @())
 {
-    $items = Get-ChildItem $source -Recurse -Include $filter -Exclude $exclude | %{ Get-RelativePath $source $_ }
+    $items = Get-ChildItem $source -Recurse -Include $filter -Exclude $exclude -Attributes !Directory | %{ Get-RelativePath $source $_ }
     ForEach ($itemSrc in $items)
     {
         $itemDest = Join-Path $dest $itemSrc
@@ -58,61 +58,46 @@ function Move-Tree ([string]$source, [string]$dest, [string[]]$filter = @("*"), 
 
 Write-Information "Moving LLVM build outputs to the expected location"
 
-Push-Location $PSScriptRoot
-$oldPath = $env:Path
+$destBase = (Join-Path $PSScriptRoot "llvm")
 
-try {
-    . .\buildutils.ps1
-
-    $destBase = (Join-Path $PSScriptRoot "llvm")
-
-    if (Test-Path $destBase) {
-        Write-Verbose "Cleaning out the old data from $($destbase)"
-        Remove-Item -Path $destbase -Recurse -Force | Out-Null
-    }
-
-    $sourceConfiguration = $Configuration
-    if ($sourceConfiguration = "Release") {
-        $sourceConfiguration = "RelWithDebInfo"
-    }
-
-    $plat = Get-Platform
-    if ($plat -eq [Platform]::Windows) {
-        $libSource = (Join-Path ($BuildRoot) ($BuildName) $sourceConfiguration "lib")
-    } else {
-        $libSource = (Join-Path ($BuildRoot) ($BuildName) "lib")
-    }
-    $libDest = (Join-Path ($destBase) ($BuildName) ($Configuration) "lib")
-    Write-Verbose "Moving built libraries from $($libSource) to $($libDest)"
-    Move-Tree $libSource $libDest 
-
-    $incSource = (Join-Path ($BuildRoot) ($BuildName) "include")
-    $incDest = (Join-Path ($destbase) ($BuildName) "include")
-    $incFilter = @( '*.h', '*.gen', '*.def', '*.inc' )
-    Write-Verbose "Moving headers from $($incSource) to $($incDest)"
-    Move-Tree $incSource $incDest ($incFilter)
-
-    $inc2Source = (Join-Path (Split-Path $BuildRoot -Parent) "include")
-    $inc2Dest = (Join-Path ($destbase) "include")
-    $inc2Exclude = @( '*.txt')
-    Write-Verbose "Moving headers from $($inc2Source) to $($inc2Dest)"
-    Copy-Tree $inc2Source $inc2Dest -exclude ($inc2Exclude)
-
-    $cfgSource = (Join-Path $BuildRoot ($BuildName) "NATIVE" "include" "llvm" "Config")
-    $cfgDest = (Join-Path ($destbase) "include" "llvm" "Config")
-    Write-Verbose "Moving config headers from $($cfgSource) to $($cfgDest)"
-    Copy-Tree $cfgSource $cfgDest
-
-    Write-Verbose "Copying OrcCBindingsStack.h"
-    $orcSource = (Join-Path $PSScriptRoot "llvm-project" "llvm" "lib" "ExecutionEngine" "Orc" "OrcCBindingsStack.h")
-    $orcDest = (Join-Path ($destBase) "lib" "ExecutionEngine" "Orc")
-    if (!(Test-Path $orcDest))
-    {
-        Write-Verbose "Creating directory $($orcDest)"
-        New-Item -Path $orcDest -ItemType "directory" -Force
-    }
-    Copy-Item -Path $orcSource -Destination $orcDest -Force
-} finally {
-    Pop-Location
-    $env:Path = $oldPath
+if (Test-Path $destBase) {
+    Write-Verbose "Cleaning out the old data from $($destbase)"
+    Remove-Item -Path $destbase -Recurse -Force | Out-Null
 }
+
+$sourceConfiguration = $Configuration
+if ($sourceConfiguration = "Release") {
+    $sourceConfiguration = "RelWithDebInfo"
+}
+
+$libSource = (Join-Path ($BuildRoot) ($BuildName) $sourceConfiguration "lib")
+$libDest = (Join-Path ($destBase) ($BuildName) ($Configuration) "lib")
+Write-Verbose "Moving built libraries from $($libSource) to $($libDest)"
+Move-Tree $libSource $libDest 
+
+$incSource = (Join-Path ($BuildRoot) ($BuildName) "include")
+$incDest = (Join-Path ($destbase) ($BuildName) "include")
+$incFilter = @( '*.h', '*.gen', '*.def', '*.inc' )
+Write-Verbose "Moving headers from $($incSource) to $($incDest)"
+Move-Tree $incSource $incDest ($incFilter)
+
+$inc2Source = (Join-Path (Split-Path $BuildRoot -Parent) "include")
+$inc2Dest = (Join-Path ($destbase) "include")
+$inc2Exclude = @( '*.txt')
+Write-Verbose "Moving headers from $($inc2Source) to $($inc2Dest)"
+Copy-Tree $inc2Source $inc2Dest -exclude ($inc2Exclude)
+
+$cfgSource = (Join-Path $BuildRoot ($BuildName) "NATIVE" "include" "llvm" "Config")
+$cfgDest = (Join-Path ($destbase) "include" "llvm" "Config")
+Write-Verbose "Moving config headers from $($cfgSource) to $($cfgDest)"
+Copy-Tree $cfgSource $cfgDest
+
+Write-Verbose "Copying OrcCBindingsStack.h"
+$orcSource = (Join-Path $PSScriptRoot "llvm-project" "llvm" "lib" "ExecutionEngine" "Orc" "OrcCBindingsStack.h")
+$orcDest = (Join-Path ($destBase) "lib" "ExecutionEngine" "Orc")
+if (!(Test-Path $orcDest))
+{
+    Write-Verbose "Creating directory $($orcDest)"
+    New-Item -Path $orcDest -ItemType "directory" -Force
+}
+Copy-Item -Path $orcSource -Destination $orcDest -Force
