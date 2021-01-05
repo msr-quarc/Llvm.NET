29cffb053efee74722b1cbe480c99de41988c557
Stefan J. Wernli <swernli@microsoft.com>
2021-01-05 13:40:40 -0800

    Try removing Antlr text replacement

--- Pack-NuGet.ps1
+++ Pack-NuGet.ps1
@@ -3,5 +3,9 @@ $buildInfo = Initialize-BuildEnvironment
 
 # Push-Location $buildInfo['NuGetOutputPath']
 # Compress-Archive -Force -Path *.* -DestinationPath (join-path $buildInfo['ArtifactDrops'] Nuget.Packages.zip)
+$dest = Join-Path $buildInfo['ArtifactDrops'] (Split-Path $buildInfo['NugetOutputPath'] -Leaf)
+if (Test-Path $dest) {
+    Remove-Item -Recurse -Force $dest
+}
 Move-Item -Force -Path $buildInfo['NugetOutputPath'] -Destination $buildInfo['ArtifactDrops']
 # Pop-Location
